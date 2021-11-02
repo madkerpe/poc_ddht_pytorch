fc3eb965c0650652eb569ab186d172bc2ea27056
madkerpe <marijn.dekerpel@ugent.be>
2021-11-02 11:25:32 +0100

    initial upload

---
+++ dynamicDeepHit.py
@@ -0,0 +1,19 @@
+from torch.nn import Linear, GRU
+from torch.nn.functional import relu
+
+_EPSILON = 1e-08
+
+##### USER-DEFINED FUNCTIONS
+def log(x):
+    return torch.log(x + _EPSILON)
+
+def div(x, y):
+    return torch.div(x, (y + _EPSILON))
+
+class sharedSubnetwork(torch.nn.Module):
+    def __init__(self):
+        torch.nn.Module.__init__(self)
+
+    def forward(self, x):
+         
+         return None

c3950c5db440ef74d9a376a199664b616fbd7be0
madkerpe <marijn.dekerpel@ugent.be>
2021-11-09 18:57:27 +0100

    make tte and longit data dependent on a latent var

--- poc_data.py
+++ poc_data.py
@@ -2,43 +2,57 @@ import numpy as np
 import torch
 
 length = 30
-num_covariates = 4 # Don't change this
+num_covariates = 3 # Don't change this
 batch_size = 16
 
 
-def generate_sample(id, event):
+def generate_sample(id, latent_variable=None):
     # Generate a longitudional data sample, with 3 covariates,
     # Temporally 30 units long, no censoring yet
     # first dimention is just an id
     # second dimension a linear increasing value
     # third dimension is the sinus of the linear increasing value
-    # TODO a fourth dimension that has all the information of the dying process
+    # TODO maybe a fourth dimension that has more information of the dying process
     # TODO add time to event to the dataloader
     # TODO left & right censoring
 
+    # There is one latent variable, a value between 0 and 1, the survival time is dependent on it AND it is present in the longitudional data by the amplitude of the sine function
+
     # dataset in DDHT looks like this for each case: 
-    #  (covariates, 
-    #   mask vectors that indicates which covariates are missing,
-    #   time-delta between each measurement
-    #   time till event
+    #  (covariates, --> 3 covariates
+    #   mask vectors that indicates which covariates are missing, --> not present here yet
+    #   time-delta between each measurement --> here it's constant
+    #   time till event --> 
     #   actual event)
 
+    # define the latent variable
+    if latent_variable == None:
+        latent_variable = torch.rand(1)
+
+    # reserve space for the data sample
     data = torch.zeros(length, num_covariates)
     data[:, 0] = id * torch.ones(length)
 
-    random_start = length + 10 * torch.randn(1)
+    # make the time horizon dependent on the latent variable
+    event_time = 1 + torch.floor(length*latent_variable)
 
-    if event == 0:
-        data[:, 1] = random_start + torch.tensor(np.linspace(0, 2 * np.pi, num=length))
-        data[:, 2] = torch.sin(data[:, 1])
+    # Add some random noise on the start of the sample
+    random_start = 10 + 10 * torch.rand(1)
+    data[:, 1] = random_start + torch.tensor(np.linspace(0, 5 * np.pi, num=length))
+    
+    # make the event dependent on the latent variable
+    if latent_variable <= 0.5:
+        event = 0
 
-    elif event == 1:
-        data[:, 1] = random_start + torch.tensor(np.linspace(0, 2 * np.pi, num=length))
+    if latent_variable > 0.5:
+        event = 1
         data[:, 2] = 2 * torch.sin(data[:, 1])
 
+    # make the longitudional data dependent on the latent variable
+    data[:, 2] = latent_variable*torch.sin(data[:, 1])
 
-    # we need a number between 1 and the time horizon to make the even happen
-    data[:, 3] = np.exp((-1)*(data[:, 1] - random_start)) - ((random_start)/(2*(length+10)))
+    if event_time > 29:
+        event_time = 29
 
     return data
 

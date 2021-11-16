164bf140c16ae54e4f63be0e3b4613c31f850575
madkerpe <marijn.dekerpel@ugent.be>
2021-11-16 20:11:15 +0100

    Debugging attention decoder

--- dynamicDeepHit.py
+++ dynamicDeepHit.py
@@ -9,24 +9,11 @@ batch_size = 16
 
 MAX_LENGTH = 30
 
-class SharedSubnetwork(torch.nn.Module):
-    def __init__(self, num_covariates=3, hidden_states=32):
-        torch.nn.Module.__init__(self)
-        self.gru = GRU(input_size=num_covariates, hidden_size=hidden_states, batch_first=True, num_layers=1)
-        self.linear = Linear(hidden_states, num_covariates)
-
-    def forward(self, x):
-        gru_output, gru_hidden = self.gru(x)
-        h = self.linear(gru_output)
-        y = relu(h)
-        
-        return y, gru_hidden
-
 class EncoderRNN(torch.nn.Module):
     def __init__(self, input_size, hidden_size):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
-        self.gru = GRU(input_size=input_size, hidden_size=hidden_size, batch_first=True, num_layers=1)
+        self.gru = GRU(input_size=input_size, hidden_size=hidden_size, batch_first=True)
         self.fc_layer = Linear(hidden_size, input_size)
 
     def forward(self, input, hidden):
@@ -45,16 +32,18 @@ class AttnDecoderRNN(torch.nn.Module):
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.max_length = max_length
-
         self.attn = Linear(self.hidden_size + output_size, self.max_length)
 
-    def forward(self, input, hidden, encoder_outputs):
+    def forward(self, last_measurement, encoder_hidden_vector):
+        #TODO Here in the decoder, we abuse the batch system
+        # Ik heb het verkeerd gebruikt, nu mapt het de sequence naar 30 waarden per hidden state, wat niet juist is...
+        input_for_attention = torch.cat((last_measurement.repeat(self.max_length,1), encoder_hidden_vector), 1)
+        importance = self.attn(input_for_attention)
+        attn_weights = softmax(importance, dim=1)
 
-        attn_weights = self.attn(torch.cat((input[0], hidden[0]), 1))
-        attn_weights = softmax(attn_weights, dim=1)
-        context_vector = torch.bmm(attn_weights.unsqueeze(0), encoder_outputs.unsqueeze(0))
+        context_vector = torch.bmm(attn_weights.unsqueeze(0), encoder_hidden_vector.unsqueeze(0))
 
-        return context_vector
+        return context_vector, attn_weights
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size)

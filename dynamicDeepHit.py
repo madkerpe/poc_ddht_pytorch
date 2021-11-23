fa0d28e65759ec02e77c9cd201cde9ae9d3e5654
madkerpe <marijn.dekerpel@ugent.be>
2021-11-23 18:16:55 +0100

    Encoder shows that it can learn, tested on a single sample

--- dynamicDeepHit.py
+++ dynamicDeepHit.py
@@ -1,33 +1,35 @@
 import torch
 from torch.nn import Linear, GRU
-from torch.nn.functional import relu, softmax
+from torch.nn.functional import relu, softmax, tanh
 
 _EPSILON = 1e-08
 length = 30
 num_covariates = 3  # This is not customisable
 batch_size = 16
 
-MAX_LENGTH = 30
-
 class EncoderRNN(torch.nn.Module):
     def __init__(self, input_size, hidden_size):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
-        self.gru = GRU(input_size=input_size, hidden_size=hidden_size, batch_first=True)
-        self.fc_layer = Linear(hidden_size, input_size)
+        self.input_size = input_size
+        self.gru = GRU(input_size, hidden_size, batch_first=True)
+        self.fc_layer_1 = Linear(hidden_size, hidden_size//2)
+        self.fc_layer_2 = Linear(hidden_size//2, hidden_size//4)
+        self.fc_layer_3 = Linear(hidden_size//4, input_size)
 
     def forward(self, input, hidden):
         output, hidden = self.gru(input, hidden)
-        output = self.fc_layer(output)
-        output = relu(output)
+        output = relu(self.fc_layer_1(output))
+        output = relu(self.fc_layer_2(output))
+        output = self.fc_layer_3(output)
 
         return output, hidden
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size)
+    def initHidden(self, device='cpu'):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
 
 class AttnDecoderRNN(torch.nn.Module):
-    def __init__(self, hidden_size, output_size, max_length=MAX_LENGTH):
+    def __init__(self, hidden_size, output_size, max_length):
         super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -40,9 +42,33 @@ class AttnDecoderRNN(torch.nn.Module):
         importance = self.attn(input_for_attention)
         attn_weights = softmax(importance, dim=0)
 
-        context_vector = attn_weights*encoder_hidden_vector
+        context_vector = torch.mm(torch.transpose(attn_weights, 0, 1), encoder_hidden_vector)
 
-        return context_vector, attn_weights
+        return context_vector
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size)
+class SharedSubnetwork(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, max_length, device):
+        super(SharedSubnetwork, self).__init__()
+        self.encoder = EncoderRNN(input_size, hidden_size)
+        self.decoder = AttnDecoderRNN(hidden_size, output_size, max_length)
+
+        self.encoder_hidden_vector = torch.zeros(max_length, self.encoder.hidden_size, device=device)
+        self.encoder_output_vector = torch.zeros(max_length, output_size, device=device)
+        self.encoder_hidden = self.encoder.initHidden(device)
+
+    def forward(self, batch, time_to_event):
+        input_length = batch.size(1)
+
+        for ei in range(input_length):
+            encoder_input = batch[0][ei].view(1,1,-1)
+            encoder_output, self.encoder_hidden = self.encoder(encoder_input, self.encoder_hidden)
+            self.encoder_hidden_vector[ei] = self.encoder_hidden[0,0].clone()
+            self.encoder_output_vector[ei] = encoder_output.clone()
+
+
+        #last_measurement_index = int(time_to_event.item())
+        last_measurement = batch[0][ei]
+
+        context_vector = self.decoder(last_measurement, self.encoder_hidden_vector)
+
+        return context_vector, self.encoder_output_vector

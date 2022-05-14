56800d5ae884408acd8032ede0d9070ee843b467
madkerpe <marijn.dekerpel@ugent.be>
2022-05-14 17:35:11 +0200

    YOU NEED INPUT SCALING RETARD

--- poc_data_v2.py
+++ poc_data_v2.py
@@ -63,8 +63,8 @@ def generate_sample():
     data[:, 0] = torch.arange(0, age, dtype=torch.float32)
     data[:, 1] = max_age*torch.ones(age)
     data[:, 2] = max_age - torch.arange(0, age, dtype=torch.float32) - 1
-    data[:, 3] = X*torch.sin(torch.arange(0, age, dtype=torch.float32) + X)
-    data[:, 4] = Y*torch.cos(torch.arange(0, age, dtype=torch.float32) + Y)
+    data[:, 3] = 100*X*torch.sin(torch.arange(0, age, dtype=torch.float32) + X)
+    data[:, 4] = 100*Y*torch.cos(torch.arange(0, age, dtype=torch.float32) + Y)
 
     # define the latent variables Z1 and Z2
     Z1 = torch.rand(1)
